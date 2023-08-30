8db16d2cdd0454360e50e4112f513c1fc2bf982e
aashishthakurcodes <j9183208@gmail.com>
2023-08-30 12:17:19 +0530

    serevr.js

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ import authRoutes from "./Routes/authRoutes.js";
 import categoryRoutes from './Routes/categoryRoutes.js'
 import productRoutes from './Routes/productRoutes.js'
 import cors from'cors'
-import path, { join } from 'path'
+import path, { dirname } from 'path';  // Fixed import statements
 import { fileURLToPath } from "url";
 
 //Configure env
@@ -16,27 +16,29 @@ dotenv.config();
 //Db call
 connectDb();
 
-const __filename=fileURLToPath(import.meta.url);
-const __dirname=path.dirname(__filename);
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);  // Changed from path.dirname
 
-//Rest ooject
+//Rest object
 const app = express();
 
-//Middlewaress
+//Middlewares
 //Using cors
 app.use(cors());
 app.use(express.json());
 app.use(morgan("dev"));
-app.use(express.static(path,join(__dirname,'./client/build')))
+
+// Serve static files from React build
+app.use(express.static(path.join(__dirname, 'client', 'build')));  // Fixed path.join
 
 //Routes
-app.use('/api/v1/auth',authRoutes)
-app.use('/api/v1/category',categoryRoutes)
-app.use('/api/v1/product',productRoutes)
+app.use('/api/v1/auth', authRoutes)
+app.use('/api/v1/category', categoryRoutes)
+app.use('/api/v1/product', productRoutes)
 
 // Rest API
-app.get('*',function(req,res){
-  res.sendFile(path.join(__dirname,'./client/build/index.html'))
+app.get('*', function (req, res) {
+  res.sendFile(path.join(__dirname, 'client', 'build', 'index.html'));  // Fixed path.join
 })
 
 const PORT = process.env.PORT || 8080;
